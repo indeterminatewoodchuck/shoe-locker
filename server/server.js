0ff5e5c617d215bbd32c03e8b9a9435a3ffc96ea
brettkan <bkfighton@gmail.com>
2015-07-14 16:48:18 -0700

    Updated to work with server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 
@@ -7,5 +8,17 @@ var app = express();
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use('/library', express.static(__dirname + '/../library'));
+
+// app.use('/library', function(req, res) {
+//   // res.sendFile(path.resolve(__dirname, '/../library/chatLibrary.js'));
+//   // res.sendFile(path.resolve('./library/library.js'));
+//   // res.sendFile('library');
+//   console.log(path.resolve('./library'));
+// });
+
+app.get('/blank', function(req, res) {
+  res.sendFile(__dirname + '/../blank.html');
+});
 
 module.exports = app;
