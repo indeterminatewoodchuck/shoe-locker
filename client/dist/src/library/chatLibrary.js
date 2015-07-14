c4bf2d9cac6a7b8c790ec78c13f55924cfff4b4f
brettkan <bkfighton@gmail.com>
2015-07-13 20:00:40 -0700

    (feat) add ability for client to connect with socket.io and iceComm

--- client/dist/src/library/chatLibrary.js
+++ client/dist/src/library/chatLibrary.js
@@ -1,23 +1,31 @@
 var chatButton = document.getElementById("chat-button");
 
 var createChatSession = function() {
+  // should we pass in company name or other identifier?
   var comm = new Icecomm('NEED AN API KEY HERE');
   var chatWindow = document.createElement('div');
   chatButton.parentNode.appendChild(chatWindow);
 
-  // socket.emit -> emit ('customerRequest')
+  // Change this to our server url
+  var socket = io('http://localhost:3000');
 
+  // will need to emit some kind of customer number?
+  socket.emit('customerRequest');
 
-  // socket.on('customerRoom') - ok to enter room
-    // comm.connect -> room that is sent back
-  // comm.on('connected') -> add video to div
-  // comm.on('disconnect') -> remove video from div
-    // turn off listeners? comm.leave?
+  socket.on('joinRoom', function(data) {
+    comm.on('connected', function(peer) {
+      chatWindow.appendChild(peer.getVideo());
+    });
+
+    comm.on('disconnect', function() {
+      comm.leave(true);
+    });
+
+    // figure out what format data is being sent back with
+    comm.connect(data.SOMETHING);
+  });
+
 
 };
 
 chatButton.addEventListener('click', createChatSession, false);
-
-
-// Need to emit 'customerRequest' in order to add to customer queue
-// listen for 'customerRoom' to get the room number
