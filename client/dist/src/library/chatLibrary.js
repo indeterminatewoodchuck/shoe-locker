97ec85f51165b8af144f7671cb04779cc03af574
brettkan <bkfighton@gmail.com>
2015-07-14 12:57:03 -0700

    (refactor) refactor client module for modularity and include socket and icecomm scripts in library

--- client/dist/src/library/chatLibrary.js
+++ client/dist/src/library/chatLibrary.js
@@ -1,8 +1,16 @@
-var chatButton = document.getElementById("chat-button");
+var head = document.getElementsByTagName('head')[0];
+var socketScript = document.createElement('script');
+var icecommScript = document.createElement('script');
+socketScript.src = 'https://cdn.socket.io/socket.io-1.3.5.js';
+icecommScript.src = 'https://cdn.icecomm.io/icecomm.js';
+head.appendChild(socketScript);
+head.appendChild(icecommScript);
+
+var chatButton = document.getElementById('chat-button');
 
 var createChatSession = function() {
   // should we pass in company name or other identifier?
-  var comm = new Icecomm('NEED AN API KEY HERE');
+  var comm = new Icecomm('ZZ2RA1DsHd9xdCqdoeJ8Wwra5A5fUKipAVrvzX6vOGHlLiAdO');
   var chatWindow = document.createElement('div');
   chatButton.parentNode.appendChild(chatWindow);
 
@@ -13,19 +21,26 @@ var createChatSession = function() {
   socket.emit('customerRequest');
 
   socket.on('joinRoom', function(data) {
-    comm.on('connected', function(peer) {
-      chatWindow.appendChild(peer.getVideo());
-    });
+    setupVideoChatListeners(comm, chatWindow, data);
+  });
+};
 
-    comm.on('disconnect', function() {
-      comm.leave(true);
-    });
-
-    // figure out what format data is being sent back with
-    comm.connect(data.SOMETHING);
+var setupVideoChatListeners = function(comm, chatWindow, data){
+  comm.on('connected', function(peer) {
+    chatWindow.appendChild(peer.getVideo());
   });
 
+  comm.on('local', function(self) {
+    chatWindow.appendChild(self.getVideo());
+  });
 
+  comm.on('disconnect', function() {
+    comm.leave(true);
+    chatWindow.remove();
+  });
+
+  // figure out what format data is being sent back with
+  comm.connect(data.SOMETHING);
 };
 
 chatButton.addEventListener('click', createChatSession, false);
