8422836c12751d8014b742e51df71f2bf98f0216
Niall O'Kane <okanenm@gmail.com>
2015-07-22 12:02:21 -0700

    (refactor) delete unnecessary code. now relying on library hosted on production

--- library/chatLibrary.js
+++ library/chatLibrary.js
@@ -6,42 +6,5 @@ icecommScript.src = 'https://cdn.icecomm.io/icecomm.js';
 head.appendChild(socketScript);
 head.appendChild(icecommScript);
 
-var chatButton = document.getElementById('chat-button');
 
-var createChatSession = function() {
-  // should we pass in company name or other identifier?
-  console.log("CREATE CHAT SESSION");
-  var comm = new Icecomm('ZZ2RA1DsHd9xdCqdoeJ8Wwra5A5fUKipAVrvzX6vOGHlLiAdO');
-  var chatWindow = document.createElement('div');
-  chatButton.parentNode.appendChild(chatWindow);
 
-  // Change this to our server url
-  var socket = io('http://localhost:3000');
-
-  // will need to emit some kind of customer number?
-  socket.emit('customerRequest', 'ShoeLocker');
-
-  socket.on('customerRoom', function(data) {
-    setupVideoChatListeners(comm, chatWindow, data);
-  });
-};
-
-var setupVideoChatListeners = function(comm, chatWindow, data){
-  comm.on('connected', function(peer) {
-    chatWindow.appendChild(peer.getVideo());
-  });
-
-  comm.on('local', function(self) {
-    chatWindow.appendChild(self.getVideo());
-  });
-
-  comm.on('disconnect', function() {
-    comm.leave(true);
-    chatWindow.remove();
-  });
-
-  // figure out what format data is being sent back with
-  comm.connect(data.SOMETHING);
-};
-
-chatButton.addEventListener('click', createChatSession, false);
